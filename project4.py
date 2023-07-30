694acb6e73114c41b389fb6f9ffbde08fdb33296
varun3834 <139316140+varun3834@users.noreply.github.com>
2023-07-30 18:40:11 +0530

    Update project4.py

--- project4.py
+++ project4.py
@@ -1,8 +1,3 @@
-# p1= open("/home/varun/Documents/study/code/29-7-2023/project2.py","r")
-# p2= open("/home/varun/Documents/study/code/29-7-2023/project3.py","r")
-# print(p1)
-# print(p2)
-
 a=int(input('count no of length: '))
 i=0
 while i<a:
